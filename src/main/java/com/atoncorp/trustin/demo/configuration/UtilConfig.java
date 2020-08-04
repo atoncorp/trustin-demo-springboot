7bf0522139d2b42fb07e2abd8b3514aef9dba7b7
trustin-dev <jclee@atoncorp.com>
2020-08-04 13:37:08 +0900

    Rest Client 변경

--- src/main/java/com/atoncorp/trustin/demo/configuration/UtilConfig.java
+++ src/main/java/com/atoncorp/trustin/demo/configuration/UtilConfig.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.Duration;
@@ -15,7 +16,9 @@ public class UtilConfig {
     RestTemplate restTemplate(){
         RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
         restTemplateBuilder.setConnectTimeout(Duration.ofSeconds(10)).setReadTimeout(Duration.ofSeconds(10));
-        return restTemplateBuilder.build();
+        RestTemplate build = restTemplateBuilder.build();
+        build.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+        return build;
     }
 
     @Bean
